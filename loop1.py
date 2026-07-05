005f877985aac7c3624e0ff04ce8efc92fa0fe58
dev-swarna <swarnamayee564@gmail.com>
2026-07-05 09:19:15 +0530

    in

--- loop1.py
+++ loop1.py
@@ -1,5 +1,22 @@
 # for i in "Swarna":
 #     print(i,end="-")
 
-for i in range(2,8):
-   print(i,i*i)
+# for i in range(2,8):
+#    print(i,i*i)
+# for i in range(2,8):
+#    print(i**2)
+
+
+# for i in range(1,11):
+#     print("5 *", i , "=" , 5*i)
+
+
+# for i in range(1,51):
+#     if i % 5 == 0:
+#         print(i)
+
+# for i in range(1,51):
+#     if 50 % i == 0:
+#         print(i)
+
+
